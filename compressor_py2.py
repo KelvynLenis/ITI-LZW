0ff21e49aeb29e4480eb94230e8612db1ab17fc4
KelvynLenis <52057929+KelvynLenis@users.noreply.github.com>
2023-05-09 15:21:00 -0300

    Add files via upload

---
+++ compressor_py2.py
@@ -0,0 +1,57 @@
+import sys
+import time
+
+def compress_lzw(file_name, k):
+    with open(file_name, 'r', encoding='iso-8859-1') as f:
+        data = f.read()
+
+    dictionary = {chr(i): i for i in range(256)}
+    dict_size = 256
+    current_string = ''
+    output = []
+    MAX_DICT_SIZE = 2**k
+
+    for char in data:
+        if current_string + char in dictionary:
+            current_string += char
+        else:
+            output.append(dictionary[current_string].to_bytes(2, byteorder='big'))
+            if dict_size < MAX_DICT_SIZE:
+                dictionary[current_string + char] = dict_size
+                dict_size += 1
+            current_string = char
+
+    if current_string in dictionary:
+        output.append(dictionary[current_string].to_bytes(2, byteorder='big'))
+    else:
+        output.append(dictionary[current_string[:-1]].to_bytes(2, byteorder='big'))
+        output.append(dictionary[current_string[-1]].to_bytes(2, byteorder='big'))
+
+    with open(f'./Output/compress/compressed{k}' + '.lzw', 'wb') as f:
+        for item in output:
+            if len(item) == 2:
+                f.write(item)
+            elif len(item) == 3:
+                MAX_INT_SIZE = 65535 # Tamanho máximo permitido para um valor inteiro de 2 bytes
+                if int.from_bytes(item, byteorder='big') <= MAX_INT_SIZE:
+                    f.write(item[:2])
+                else:
+                    parts = []
+                    value = int.from_bytes(item, byteorder='big')
+                    while value > 0:
+                        part = value % MAX_INT_SIZE
+                        parts.append(part.to_bytes(2, byteorder='big'))
+                        value = (value - part) // MAX_INT_SIZE
+                    f.write(reversed(parts))
+
+    print(f"Arquivo comprimido salvo na pasta Output.")
+
+if __name__ == "__main__":
+    # file_name = sys.argv[1]
+    file_name = './Input/corpus16MB.txt'
+    for k in range(9, 17):
+        start_time = time.time()  # marca o tempo inicial
+        compress_lzw(file_name, k)
+        end_time = time.time()  # marca o tempo final
+        elapsed_time = end_time - start_time  # calcula o tempo decorrido
+        print(f"Iteração {k}: {elapsed_time:.6f} segundos")
---
+++ decompressor.py
@@ -0,0 +1,56 @@
+import time
+import sys
+
+def decompress_lzw(input_file, k):
+    # Inicializa o dicionário com os caracteres ASCII
+    dictionary = {i: chr(i) for i in range(256)}
+    dict_size = 256
+
+    current_string = ''
+    output = []
+    MAX_DIC_SIZE = 2**k
+
+    # Lê o arquivo de entrada como uma sequência de bytes
+    with open(input_file, "rb") as f:
+        compressed_data = f.read()
+
+    # Converte a sequência de bytes em uma sequência de números inteiros
+    compressed_numbers = []
+    for i in range(0, len(compressed_data), 2):
+        compressed_numbers.append(int.from_bytes(compressed_data[i:i+2], byteorder='big'))
+
+    # Inicializa o processo de descompressão
+    current_string = chr(compressed_numbers.pop(0))
+    output = [current_string]
+
+    # Descomprime os dados usando o algoritmo LZW
+    for num in compressed_numbers:
+        if num in dictionary:
+            entry = dictionary[num]
+        elif num == dict_size:
+            entry = current_string + current_string[0]
+        else:
+            raise ValueError("Compressed data is invalid")
+
+        output.append(entry)
+
+        # Adiciona uma nova entrada ao dicionário, se ainda houver espaço
+        if dict_size < MAX_DIC_SIZE:
+            dictionary[dict_size] = current_string + entry[0]
+            dict_size += 1
+
+        current_string = entry
+
+    # Escreve a sequência de caracteres descomprimidos em um arquivo de saída
+    output_file = input_file.split(".")[0] + "_decompressed.txt"
+    with open(f'./Output/decompress/decompress{k}', "w", encoding='utf-8') as f:
+        f.write("".join(output))
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    for k in range(9, 17):
+        start_time = time.time()  # marca o tempo inicial
+        decompress_lzw(file_name, k)
+        end_time = time.time()  # marca o tempo final
+        elapsed_time = end_time - start_time  # calcula o tempo decorrido
+        print(f"Iteração {k}: {elapsed_time:.6f} segundos")
